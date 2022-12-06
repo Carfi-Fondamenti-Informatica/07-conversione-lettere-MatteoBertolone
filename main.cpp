4f83efdf66ad1e050e64e1e1ba0cb20977119fde
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-12-06 11:05:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char c;
+    cin>>c;
+    if(caratteri(c))
+        cout<<c<<endl;
+    else
+        cout<<"errore"<<endl;
+    return 0;
 }
