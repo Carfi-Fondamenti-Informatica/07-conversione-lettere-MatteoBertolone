5591d37a65ca995f95d241220b7c9779552b9070
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-12-06 11:04:45 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include "lib.h"
+bool caratteri(char &a){
+    if((a>=65 && a<=90) || (a>=97 && a<=122)){
+        if(a>=65 && a<=90)
+            a+=32;
+        else
+            a-=32;
+        return true;
+    }else
+        return false;
+}
